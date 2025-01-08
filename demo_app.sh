ea399f0a3aee1f7454e6bf0bebcfdfc946ed5a4d
Barry Walsh <barry.walsh@changingenergy.tech>
2025-01-08 18:30:03 +0000

    updated shortcut shell script to run everything

--- demo_app.sh
+++ demo_app.sh
@@ -1,3 +1,14 @@
+# kill all running python processes - use sudo if need admin password 
+killall Python .
+
+#close down problem open ports (5173 I think is vite default, 5002 is flask but *check* dont close this down as causes ngrok to boot out ??)
+# see https://stackoverflow.com/questions/38425334/close-many-ports-at-once
+# problem potys below *refactor* to a list of ports
+for i in {5170..5180}
+do
+   kill -kill `lsof -t -i tcp:$i`
+done
+
 # 1. NLP to SQL
 # open chatbot in background and chainlit multi-modal UI with audio support then run flask
 cd "/Users/barry.walsh/rotaready/rr_repos/predictive-demo-app/apps/genai-vite/ldfe-genai-demo" 
@@ -11,4 +22,10 @@ chainlit run app.py -w -h &
 
 # 3. run this app
 cd "/Users/barry.walsh/rotaready/rr_repos/predictive-demo-app" 
-flask run -h localhost -p 5002
+flask run -h localhost -p 5002 &
+
+sleep 5  # Waits 10 seconds for app to load
+
+# 4. open chrome browser for app
+open -a "Google Chrome" \
+  --args --new-window https://90dc-212-54-135-152.ngrok-free.app/index &
